fd7f5e50b4a5fd496438e2ad6143750697851656
apoko <apokora@itsap.pl>
2023-07-31 22:19:10 +0200

    Integrated Bootstrap framework

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/styles.scss";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider } from "react-router-dom"
+import "./styles/styles.scss"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.esm.min.js"
+import { router } from "./router"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 )
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import { Button } from "bootstrap";
+
+export const HomePage = () =>{
+  return (
+    <div>
+     <button type="button" class="btn btn-primary">Primary</button>
+      tekst
+    </div>
+  );
+}
